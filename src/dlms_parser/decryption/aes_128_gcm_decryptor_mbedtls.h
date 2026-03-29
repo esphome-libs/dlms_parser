9d17074c2d4d46fc25140dfd7e1f70c6987330a5
PolarGoose <35307286+PolarGoose@users.noreply.github.com>
2026-03-29 20:41:47 +0200

    Improve Aes128GcmDecryptorMbedTls

--- src/dlms_parser/decryption/aes_128_gcm_decryptor_mbedtls.h
+++ src/dlms_parser/decryption/aes_128_gcm_decryptor_mbedtls.h
@@ -13,11 +13,12 @@ public:
   ~Aes128GcmDecryptorMbedTls() override { mbedtls_gcm_free(&gcm); }
 
   void set_decryption_key(const Aes128GcmDecryptionKey& key) override {
-    mbedtls_gcm_setkey(&gcm, MBEDTLS_CIPHER_ID_AES, key.data(), 128);
-    _has_key = true;
+    _has_key = mbedtls_gcm_setkey(&gcm, MBEDTLS_CIPHER_ID_AES, key.data(), 128) == 0;
   }
 
   bool decrypt_in_place(const std::span<uint8_t> iv, std::span<uint8_t> cipher) override {
+    if (!_has_key) { return false; }
+
     unsigned char tag[12]; // dummy tag to satisfy the API
     return mbedtls_gcm_crypt_and_tag(/* ctx     */ &gcm,
                                      /* mode    */ MBEDTLS_GCM_DECRYPT,
